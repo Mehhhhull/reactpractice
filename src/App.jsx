1309b8315ac5749c64506b8cef7e6c42683b3cfd
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-07-27 00:20:56 +0530

    Made other sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Post from './components/Post'
+import RightBar from './components/RightBar'
 
 const App = () => {
   return (
    <>
    <Navbar/>
+   
    </>
   )
 }
