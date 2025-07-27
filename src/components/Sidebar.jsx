5dde391421095cfdf03f812a749a9e87a316625f
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-07-27 21:17:23 +0530

    Left

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { FaHome, FaBell, FaComments, FaWallet, FaUser } from "react-icons/fa";
+import { FaBagShopping} from "react-icons/fa6";
 
 const Sidebar = () => {
   return (
-    <div>
-      bzges
+    <div className="side_bar_content">
+      <div className="top_content">
+        <div className="items"><FaHome /> Home</div>
+        <div className="items"><FaBell /> Notification</div>
+        <div className="items"><FaBagShopping /> Shop</div>
+        <div className="items"><FaComments /> Conversation</div>
+        <div className="items"><FaWallet /> Wallet</div>
+        <div className="items"> Subscription</div>
+        <div className="items"><FaUser /> My Profile</div>
+      </div>
+      <div className="bottom_content items">
+        <span>Log Out</span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
+
